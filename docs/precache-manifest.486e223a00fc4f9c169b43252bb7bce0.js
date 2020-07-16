b1018a5dc90bde96debee99c098f5552a9d38688
Ilia Kamilov <iliakmlv@gmail.com>
2020-07-16 20:28:51 +0300

    docs folder commit

---
+++ docs/precache-manifest.486e223a00fc4f9c169b43252bb7bce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98de3504011d25b107b204c35a122ab8",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "a96e6022b30169fab2f5",
+    "url": "/todo-list/static/css/main.03e227db.chunk.css"
+  },
+  {
+    "revision": "83c0b1c47e6647ef1535",
+    "url": "/todo-list/static/js/2.2af2cd39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.2af2cd39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96e6022b30169fab2f5",
+    "url": "/todo-list/static/js/main.6c6871a9.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
